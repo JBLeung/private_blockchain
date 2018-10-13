02b6c232c026f1189df1c4bdd845817c6d5e49b7
JB <jbleunghk@gmail.com>
2018-10-13 16:26:06 +0800

    remove dummy testing code to create dummy block

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import {Block, Blockchain} from './simpleChain'
+import API from './api'
 
 const start = async () => {
-  const blockchain = new Blockchain()
-
-  const theLoop = (i) => {
-    setTimeout(() => {
-      const blockTest = new Block(`Test Block - ${i + 1}`)
-      blockchain.addBlock(blockTest).then((result) => {
-        console.log(result)
-        i += 1
-        if (i < 10) theLoop(i)
-      })
-    }, 10000)
-  }
-  theLoop(0)
+  API.init(8000)
 }
 
 start()
